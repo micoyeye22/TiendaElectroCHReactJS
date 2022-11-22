ded79f29a463df016d73a6e6704d9d8768137147
Federico Ayme <federico.ayme@gmail.com>
2022-11-22 19:02:48 -0300

    PreEntrega1+Ayme

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CartWidget from "./CartWidget";
+
+const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src="images/logo.png" alt="TiendaElectro" width="105" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">Productos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Locales</a>
+                            </li>
+                            <li className="nav-item">   
+                                <a className="nav-link" href="#">Nostros</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <CartWidget />
+                </div>
+            </nav>
+        </div>
+        
+    )
+}
+
+export default NavBar;
